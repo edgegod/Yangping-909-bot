7e7a1070ddc117748a350fb2476e74f4422b4d0c
by-debug <68426281+by-debug@users.noreply.github.com>
2020-08-27 13:08:53 +0800

    Add Scrapping
    Now you can use !technews to get the technology news from buzzorange.com today.

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -10,5 +12,12 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{round(bot.latency*1000)}(ms)")
+
+@bot.command()
+async def technews(ctx):
+    web_data = requests.get('https://buzzorange.com/techorange/')
+    soup = BeautifulSoup(web_data.text,"html.parser")
+    for news in soup.find(class_='site-main post-listing').findAll(class_='entry-header'):
+        await ctx.send(news.find('time').string+':'+news.find('a').string)
 
 bot.run("NzQ3NzA0MTE0NTAyMzAzODM3.X0SvuA.XaTyeUJgjgEJxIFIS-T77q5cQC8")
